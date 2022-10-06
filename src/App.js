42372943f7dffe9d266a41ba6dfa2d54af134441
German Barborini <germanbarborini@gmail.com>
2022-10-06 16:11:22 -0300

    Footer Completo
    Foooter Completo

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ import Footer from "./components/organisms/Footer";
 function App() {
   return (
     <>
-    <h1 className="text-3xl text-secondaryColor font-bold underline">
-    Hello world!
-    </h1>
-    <Footer/>
     </>
   );
 }
