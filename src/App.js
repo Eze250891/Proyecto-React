689e1485c5624093a658ee0b47ed9b50b6e275e7
German Barborini <germanbarborini@gmail.com>
2022-10-11 15:30:33 -0300

    Prueba Carrousel
    Prueba de carrusel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Main from "./components/atoms/main";
-import Navbar from "./components/atoms/navbar";
-import Footer from "./components/organisms/footer";
-import Seccion3 from './components/atoms/seccion3.jsx';
-import Seccion5 from './components/atoms/seccion5.jsx';
+
+import TestCarrousel from "./components/molecules/TestCarrousel";
+
+
+
+
+
 
 
 
@@ -10,11 +12,8 @@ import Seccion5 from './components/atoms/seccion5.jsx';
 function App() {
   return (
     <>
-    <Main/>
-    <Navbar/>
-    <Seccion3 />
-    <Seccion5 />
-    <Footer/>
+    
+    <TestCarrousel/>
     </>
   );
 }
