5fb8931531eaa35c74eb4cf60862ba2127f6eaec
unknown <ontiverosfacundo@gmail.com>
2022-10-13 01:17:22 -0300

    se creo el componente Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import CardSeccion from './components/CardSeccion.jsx';
+import CardSeccion1 from './components/CardSeccion1.jsx';
 
 
 
@@ -8,10 +8,7 @@ import CardSeccion from './components/CardSeccion.jsx';
 function App() {
   return (
     <>  
-      <div id='seccion1' className='bg-[#957C65] h-min-screen min-w-min items-center justify-center flex flex-col flex-wrap pb-44'>
-      <h2 className='text-center font-bold my-14 text-6xl text-[#fefce8] capitalize'>últimas ofertas</h2>
-      <CardSeccion />
-      </div>
+      <CardSeccion1 />
     </>
   );
 }
